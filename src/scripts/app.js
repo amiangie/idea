24cbd292510dd6e445cd95a1d1f76bc8c19cfe06
Angie K. Titova <angie.badfox@gmail.com>
2018-04-21 21:53:21 +0300

    [JS] Add indeterminate checkbox functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,10 @@
   Author: angie
  */
 
+// -------------------------
+// Accordion 
+// -------------------------
+
 const accordionButtons = document.querySelectorAll('.js-accordion-trigger');
 
 [...accordionButtons].forEach(accordionButton => {
@@ -35,58 +39,29 @@ function collapseOtherPanels(ele) {
 
 
 
+// -------------------------
+// Indeterminate checkboxes
+// -------------------------
 
+const checkboxGroups = document.querySelectorAll('.js-checkbox-group');
 
-
-//$('input[type="checkbox"]').on('change', function(e) {
-//
-//  
-//  
-//  var checked = $(this).prop("checked"),
-//      container = $(this).parent(),
-//      siblings = container.siblings();
-//
-//  container.find('input[type="checkbox"]').prop({
-//    indeterminate: false,
-//    checked: checked
-//  });
-//
-//  function checkSiblings(el) {
-//
-//    var parent = el.parent().parent(),
-//        all = true;
-//
-//    el.siblings().each(function() {
-//      return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
-//    });
-//
-//    if (all && checked) {
-//
-//      parent.children('input[type="checkbox"]').prop({
-//        indeterminate: false,
-//        checked: checked
-//      });
-//
-//      checkSiblings(parent);
-//
-//    } else if (all && !checked) {
-//
-//      parent.children('input[type="checkbox"]').prop("checked", checked);
-//      parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
-//      
-//      
-//      checkSiblings(parent);
-//
-//    } else {
-//
-//      el.parents("li").children('input[type="checkbox"]').prop({
-//        indeterminate: true,
-//        checked: false
-//      });
-//
-//    }
-//
-//  }
-//
-//  checkSiblings(container);
-//});
+[...checkboxGroups].forEach(checkboxGroup => {
+  const checkboxes = checkboxGroup.querySelectorAll('input[type="checkbox"]:not([aria-controls])');  
+  const checkallBox = checkboxGroup.querySelectorAll('input[type="checkbox"][aria-controls]')[0];
+  const checkboxesCount = checkboxes.length;
+  
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', function(event) {
+      let checkedCount = checkboxGroup.querySelectorAll('input[type="checkbox"]:not([aria-controls]):checked').length;
+      
+      checkallBox.checked = checkedCount > 0;
+      checkallBox.indeterminate = checkedCount > 0 && checkedCount < checkboxes.length;
+    });
+  });
+  
+  checkallBox.addEventListener('click', function(event) {
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = this.checked;
+    });
+  });
+});
